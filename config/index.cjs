10867ab3b98e2eb2f2b3f83ffda4745c776133ff
SAF <36628824+SongAoFei@users.noreply.github.com>
2023-12-30 18:42:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -104,7 +104,7 @@ USE_PASSAGE: 'server-chan',
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT234805TBe3KiwfEAVMwPKKba5AXh7Mu',
+      id: 'SCT234815TdLZSpCweEqN5bRDMmcbWLzXr',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD

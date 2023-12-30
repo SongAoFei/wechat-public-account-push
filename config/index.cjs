d2b1ef73e9f3013084a9833adee76dd57b72fcde
SAF <36628824+SongAoFei@users.noreply.github.com>
2023-12-30 16:48:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -97,7 +97,7 @@ USE_PASSAGE: 'server-chan',
 
 
   PROVINCE: '广东',
-  CITY: '惠州',
+  CITY: '佛山',
 
   USERS: [
     {
@@ -108,24 +108,24 @@ USE_PASSAGE: 'server-chan',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '09-24',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2001', date: '08-15',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*生日', name: '宋傲飞', year: '2000', date: '06-06',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2021', date: '11-14',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2021-11-14' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],

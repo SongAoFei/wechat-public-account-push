5cc883192ea56f193ffb9607b386008511026864
SAF <36628824+SongAoFei@users.noreply.github.com>
2023-12-30 15:53:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7jhn6togvm-fU6WWHQTYxn3V4gc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'DO06I7TGId6ENMM3_zlQ91rsS-DLTYV1J34ToSG45Ek',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
